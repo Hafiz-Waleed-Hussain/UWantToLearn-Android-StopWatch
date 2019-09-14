f89a342fc995ae31b184b83bf340910675a6a623
Hafiz Waleed Hussain <hafizwaleedhussain@gmail.com>
2019-09-14 18:02:55 +0800

    Android Stop Watch

---
+++ app/src/main/java/com/uwantolearn/uwanttolearn_android_stopwatch/StopWatchMainActivity.kt
@@ -0,0 +1,12 @@
+package com.uwantolearn.uwanttolearn_android_stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StopWatchMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
