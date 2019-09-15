dfdf94630c15f83642c10217df60dae776caa97b
Hafiz Waleed Hussain <hafizwaleedhussain@gmail.com>
2019-09-15 15:49:22 +0800

    TimeAlgorithm Implementation Complete in main function

--- app/src/main/java/com/uwantolearn/uwanttolearn_android_stopwatch/StopWatchMainActivity.kt
+++ app/src/main/java/com/uwantolearn/uwanttolearn_android_stopwatch/StopWatchMainActivity.kt
@@ -2,11 +2,21 @@ package com.uwantolearn.uwanttolearn_android_stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
 
 class StopWatchMainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+
+fun main() {
+    Observable.interval(0, 1, TimeUnit.MILLISECONDS)
+        .takeWhile { it <= 3600L }
+        .map { seconds -> "${seconds / 60} : ${seconds % 60}" }
+        .subscribe(::println)
+
+    while (true);
 }
