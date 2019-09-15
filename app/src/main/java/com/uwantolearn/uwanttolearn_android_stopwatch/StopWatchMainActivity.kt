2b2601dfc9ca902788ff28cd7c3a9b2a67cea63e
Hafiz Waleed Hussain <hafizwaleedhussain@gmail.com>
2019-09-15 16:21:19 +0800

    Start and Reset implementation Complete by using PublishSubject

--- app/src/main/java/com/uwantolearn/uwanttolearn_android_stopwatch/StopWatchMainActivity.kt
+++ app/src/main/java/com/uwantolearn/uwanttolearn_android_stopwatch/StopWatchMainActivity.kt
@@ -3,20 +3,24 @@ package com.uwantolearn.uwanttolearn_android_stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
 class StopWatchMainActivity : AppCompatActivity() {
+
+    private val clickEmitterSubject = PublishSubject.create<Boolean>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
 
-fun main() {
-    Observable.interval(0, 1, TimeUnit.MILLISECONDS)
+        startButton.setOnClickListener { clickEmitterSubject.onNext(true) }
+        resetButton.setOnClickListener { clickEmitterSubject.onNext(false) }
+    }
+
+    fun timerObservable(): Observable<String> = Observable.interval(0, 1, TimeUnit.MILLISECONDS)
         .takeWhile { it <= 3600L }
         .map { seconds -> "${seconds / 60} : ${seconds % 60}" }
-        .subscribe(::println)
-
-    while (true);
 }
+
